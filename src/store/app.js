6c20f053f4130c27fcff66c537eb1ea094a42f2f
Andre Langer <aldev@langer.guru>
2023-05-11 12:19:14 +0200

    payment methods added

--- src/store/app.js
+++ src/store/app.js
@@ -11,10 +11,24 @@ export const useAppStore = defineStore('app', {
     cart: {
       amount: 1695,
       currency: 'EUR', // 3 Letter ISO code https://www.iso.org/iso-4217-currency-codes.html
-    }
+    },
+    paymentMethods: [
+      { id: 'sepa', label: 'Sepa Lastschrift' },
+      { id: 'bank-transfer', label: 'Überweisung' },
+    ]
   }),
   getters: {
     getCheckoutId: (state) => state.checkoutId,
+    paymentMethodLabelById: (state) => {
+      return (id) => {
+        const item = state.paymentMethods.find(el => el.id === id)
+        if ( item ){
+          return item.label
+        } else {
+          return ' - not found - '
+        }
+      }
+    }
   },
   actions: {
     setCheckoutId(id) {
